ae1c1f72de047053ac697aeb94279440203d7367
Prashant Sharma <prashant100ji@gmail.com>
2026-06-03 20:20:42 +0530

    add new domains

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins 'http://localhost:5173'
-    origins 'https://salespoints.netlify.app'
+    origins 'https://salespoints.netlify.app',
+            'https://salespoints.in',
+            'https://www.salespoints.in'
 
     resource "*",
       headers: :any,
