ca3ad276ab7a5c9e0edeb52a646aad921d31d40a
Prashant Sharma <prashant100ji@gmail.com>
2026-06-06 23:03:11 +0530

    changes admin dashboard

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173'
-    # origins 'https://salespoints.netlify.app',
-    #         'https://salespoints.in',
-    #         'https://www.salespoints.in'
+    # origins 'http://localhost:5173'
+    origins 'https://salespoints.netlify.app',
+            'https://salespoints.in',
+            'https://www.salespoints.in'
 
     resource "*",
       headers: :any,
