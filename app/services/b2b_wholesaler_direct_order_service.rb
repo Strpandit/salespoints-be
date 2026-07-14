eb993773195df6d6ce6f8825bcba8297d984e533
Prashant Sharma <prashant100ji@gmail.com>
2026-07-14 22:20:59 +0530

    change payment status

--- app/services/b2b_wholesaler_direct_order_service.rb
+++ app/services/b2b_wholesaler_direct_order_service.rb
@@ -46,7 +46,7 @@ class B2bWholesalerDirectOrderService
         buyer_dealer_id: @buyer.id,
         seller_dealer_id: @seller.id,
         request_status: "pending_request",
-        status: "pending_request",
+        status: "pending_payment",
         requested_at: Time.current,
         requested_radius_km: @requested_radius_km,
         latitude: @latitude,
